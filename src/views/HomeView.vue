a68a55e2b41b09d1764d880b092144b8e7c12966
garcondeau <garcondeau@icloud.com>
2023-02-25 13:13:27 -0600

    added scss support; cleaned unused files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomePage from '../components/Home/HomePage.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomePage />
   </main>
 </template>
